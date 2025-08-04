fead3899dcf3956c5d3e285dc424004ff6709390
nadav <nadavm.7703@gmail.com>
2025-08-04 22:32:43 +0300

    Initial public commit

--- src/main/java/com/handson/searchengine/crawler/Crawler.java
+++ src/main/java/com/handson/searchengine/crawler/Crawler.java
@@ -4,6 +4,7 @@ import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.handson.searchengine.kafka.Producer;
 import com.handson.searchengine.model.*;
+import com.handson.searchengine.util.ElasticSearch;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.jsoup.Jsoup;
@@ -30,6 +31,8 @@ public class Crawler {
     ObjectMapper om;
     @Autowired
     Producer producer;
+    @Autowired
+    ElasticSearch elasticSearch;
 
 
     protected final Log logger = LogFactory.getLog(getClass());
@@ -44,6 +47,7 @@ public class Crawler {
         setCrawlStatus(crawlId, CrawlStatus.of(rec.getDistance(), rec.getStartTime(), 0,stopReason));
         if(stopReason==null){
             Document webPageContent = Jsoup.connect(rec.getUrl()).get();
+            indexElasticSearch(rec,webPageContent);
             List<String> innerUrls = extractWebPageUrls(rec.getBaseUrl(), webPageContent);
             addUrlsToQueue(rec, innerUrls, rec.getDistance() + 1);
 
@@ -77,6 +81,12 @@ public class Crawler {
         logger.info(">> extracted->" + links.size() + " links");
 
         return links;
+    }
+    private void indexElasticSearch(CrawlerRecord rec, Document webPageContent) {
+        logger.info(">> adding elastic search for webPage: " + rec.getUrl());
+        String text = String.join(" ", webPageContent.select("a[href]").eachText());
+        UrlSearchDoc searchDoc = UrlSearchDoc.of(rec.getCrawlId(), text, rec.getUrl(), rec.getBaseUrl(), rec.getDistance());
+        elasticSearch.addData(searchDoc);
     }
 
     private void initCrawlInRedis(String crawlId) throws JsonProcessingException {
